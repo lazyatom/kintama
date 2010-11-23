379df611f557a15f7ecc5b4691ddaed006a8749c
James Adam <james@lazyatom.com>
2010-11-23 21:16:26 +0000

    Space this out now it's got room to move.

--- jtest.rb
+++ jtest.rb
@@ -7,25 +7,31 @@ class Context
     @subcontexts = []
     @parent = parent
   end
+
   def run
     instance_eval(&@block)
     @subcontexts.each { |s| s.run }
   end
+
   def context(name, &block)
     @subcontexts << self.class.new(self, &block)
   end
+
   def setup(&setup_block)
     @setup_block = setup_block
   end
+
   def run_setups(environment)
     @parent.run_setups(environment) if @parent
     environment.instance_eval(&@setup_block) if @setup_block
   end
+
   def should(name, &block)
     environment = TestEnvironment.new(self)
     run_setups(environment)
     environment.instance_eval(&block)
   end
+
   def passed?
     @failures.empty? && @subcontexts.inject(true) { |result, s| result && s.passed? }
   end
@@ -34,11 +40,13 @@ class Context
     def initialize(context)
       @context = context
     end
+
     def assert(expression, message=nil)
       unless expression
         @context.failures << message
       end
     end
+
     def assert_equal(expected, actual)
       assert actual == expected, "Expected #{expected.inspect} but got #{actual.inspect}"
     end
