0442091638d14a65ad3e55b8153a4b4dfc177400
James Adam <james@lazyatom.com>
2010-11-23 20:24:25 +0000

    Now we can tell if tests pass or fail - ish. Some obvious bugs though.

--- test/jtest_test.rb
+++ test/jtest_test.rb
@@ -1,10 +1,20 @@
 require 'test/unit'
 
 class Context
+  def initialize(&block)
+    @block = block
+  end
   def run
+    instance_eval(&@block)
+  end
+  def should(name, &block)
+    instance_eval(&block)
+  end
+  def assert(expression)
+    @passed = expression
   end
   def passed?
-    true
+    @passed
   end
 end
 
@@ -19,9 +29,19 @@ class JTestTest < Test::Unit::TestCase
     assert x.passed?
   end
 
+  def test_should_fail_when_all_tests_fail
+    x = context "Given something" do
+      should "work" do
+        assert false
+      end
+    end
+    x.run
+    assert !x.passed?
+  end
+
   private
 
   def context(name, &block)
-    Context.new
+    Context.new(&block)
   end
 end
