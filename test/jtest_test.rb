a5c61dbbcb4b3fed38dceead60979c4ec86f9d96
James Adam <james@lazyatom.com>
2010-11-23 20:26:07 +0000

    Now we can tell whether a single test failed.

--- test/jtest_test.rb
+++ test/jtest_test.rb
@@ -3,6 +3,7 @@ require 'test/unit'
 class Context
   def initialize(&block)
     @block = block
+    @passed = true
   end
   def run
     instance_eval(&@block)
@@ -11,7 +12,7 @@ class Context
     instance_eval(&block)
   end
   def assert(expression)
-    @passed = expression
+    @passed = @passed && expression
   end
   def passed?
     @passed
@@ -33,6 +34,19 @@ class JTestTest < Test::Unit::TestCase
     x = context "Given something" do
       should "work" do
         assert false
+      end
+    end
+    x.run
+    assert !x.passed?
+  end
+
+  def test_should_fail_when_any_tests_fail
+    x = context "Given something" do
+      should "work" do
+        assert false
+      end
+      should "also work" do
+        assert true
       end
     end
     x.run
