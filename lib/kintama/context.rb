53e6617031b96ba7504cc750ce7e0106b3d5e4c2
James Adam <james@lazyatom.com>
2010-12-09 18:00:53 +0000

    Rearrange methods to make the expected 'public' API more obvious.

--- lib/kintama/context.rb
+++ lib/kintama/context.rb
@@ -14,66 +14,20 @@ module Kintama
       instance_eval(&block)
     end
 
-    def full_name
-      if @name
-        [@parent ? @parent.full_name : nil, @name].compact.join(" ")
-      else
-        nil
-      end
-    end
-
-    def run(runner=nil)
-      runner.context_started(self) if runner
-      tests.each { |t| t.run(runner) }
-      subcontexts.each { |s| s.run(runner) }
-      runner.context_finished(self) if runner
-      passed?
-    end
-
-    def add_subcontext(subcontext)
-      @subcontexts[subcontext.name] = subcontext
-      @subcontexts[methodize(subcontext.name)] = subcontext
-    end
-
+    # Define the setup for this context.
+    # It will also be run for any subcontexts, before their own setup blocks
     def setup(&setup_block)
       @setup_block = setup_block
     end
 
-    def run_setups(environment)
-      @parent.run_setups(environment) if @parent
-      include_modules(environment)
-      environment.instance_eval(&@setup_block) if @setup_block
-    end
-
+    # Define the teardown for this context.
+    # It will also be run for any subcontexts, after their own teardown blocks
     def teardown(&teardown_block)
       @teardown_block = teardown_block
     end
 
-    def run_teardowns(environment)
-      environment.instance_eval(&@teardown_block) if @teardown_block
-      @parent.run_teardowns(environment) if @parent
-    end
-
-    def should(name, &block)
-      add_test("should " + name, &block)
-    end
-
-    def it(name, &block)
-      add_test("it " + name, &block)
-    end
-
-    def test(name, &block)
-      add_test(name, &block)
-    end
-
-    def passed?
-      failures.empty?
-    end
-
-    def failures
-      tests.select { |t| !t.passed? } + subcontexts.map { |s| s.failures }.flatten
-    end
-
+    # Include a module in this context. Methods on that module will be available during
+    # setup, tests and teardowns in this and any subcontexts.
     def include(mod=nil, &block)
       if mod.nil?
         mod = Module.new
@@ -82,14 +36,54 @@ module Kintama
       @modules << mod
     end
 
+    # An alias for include - I'll probably remove this.
     def helpers(&block)
-      mod = Module.new
-      mod.class_eval(&block)
-      @modules << mod
+      self.include(nil, &block)
     end
 
-    def include_modules(environment)
-      @modules.each { |mod| environment.extend(mod) }
+    # Define a test to run in this context. The test name will start with "should "
+    def should(name, &block)
+      add_test("should " + name, &block)
+    end
+
+    # Define a test to run in this context. The test name will start with "it "
+    def it(name, &block)
+      add_test("it " + name, &block)
+    end
+
+    # Define a test to run in this context.
+    def test(name, &block)
+      add_test(name, &block)
+    end
+
+    # Returns the full name of this context, taking any parent contexts into account
+    def full_name
+      if @name
+        [@parent ? @parent.full_name : nil, @name].compact.join(" ")
+      else
+        nil
+      end
+    end
+
+    # Runs all tests in this context and any subcontexts.
+    # Returns true if all tests passed; otherwise false
+    def run(runner=nil)
+      runner.context_started(self) if runner
+      tests.each { |t| t.run(runner) }
+      subcontexts.each { |s| s.run(runner) }
+      runner.context_finished(self) if runner
+      passed?
+    end
+
+    # Returns true if this context has no known failed tests.
+    def passed?
+      failures.empty?
+    end
+
+    # Returns an array of tests in this and all subcontexts which failed in
+    # the previous run
+    def failures
+      tests.select { |t| !t.passed? } + subcontexts.map { |s| s.failures }.flatten
     end
 
     def [](name)
@@ -115,10 +109,20 @@ module Kintama
     end
 
     def respond_to?(name)
-      @subcontexts[name] != nil || 
-      @tests[name] != nil || 
+      @subcontexts[name] != nil ||
+      @tests[name] != nil ||
       super ||
       (@parent ? @parent.respond_to?(name) : false)
+    end
+
+    # Returns all subcontexts of this context, in alphabetical order
+    def subcontexts
+      @subcontexts.values.uniq.sort_by { |c| c.name }
+    end
+
+    # Returns all tests defined in this context (but not subcontexts), in alphabetical order
+    def tests
+      @tests.values.uniq.sort_by { |t| t.name }
     end
 
     def inspect
@@ -127,12 +131,22 @@ module Kintama
       "<Context:#{@name.inspect} @tests=#{test_names.inspect} @subcontexts=#{context_names.inspect}>"
     end
 
-    def subcontexts
-      @subcontexts.values.uniq.sort_by { |c| c.name }
+    def run_setups(environment)
+      @parent.run_setups(environment) if @parent
+      include_modules(environment)
+      environment.instance_eval(&@setup_block) if @setup_block
     end
 
-    def tests
-      @tests.values.uniq.sort_by { |t| t.name }
+    def run_teardowns(environment)
+      environment.instance_eval(&@teardown_block) if @teardown_block
+      @parent.run_teardowns(environment) if @parent
+    end
+
+    protected
+
+    def add_subcontext(subcontext)
+      @subcontexts[subcontext.name] = subcontext
+      @subcontexts[methodize(subcontext.name)] = subcontext
     end
 
     private
@@ -141,6 +155,10 @@ module Kintama
       test = Test.new(name, self, &block)
       @tests[methodize(name)] = test
       @tests[name] = test
+    end
+
+    def include_modules(environment)
+      @modules.each { |mod| environment.extend(mod) }
     end
 
     def methodize(name)
